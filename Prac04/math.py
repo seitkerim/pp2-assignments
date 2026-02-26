2f3635d7c76a7e2ae8a7bfdaaadbee5536879aee
killercraftcryt <killercraftcryt@gmail.com>
2026-02-27 00:20:37 +0500

    Math and random operations

---
+++ Prac04/math.py
@@ -0,0 +1,25 @@
+#1
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x) # 5
+print(y) # 25
+
+#2
+x = abs(-7.25)
+
+print(x) #module
+
+#3
+x = pow(4, 3)
+
+print(x) # 4*4*4
+
+#4
+import math
+
+x = math.sqrt(64)
+
+print(x) #8.0
+
+
